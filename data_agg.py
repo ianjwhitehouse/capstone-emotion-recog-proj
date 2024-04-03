c656ffb7f3d5b2e68964e326315753f16f290bb0
bte_lead <ian.whitehouse88@gmail.com>
2024-04-03 16:34:45 -0400

    Changed styling of application and how events are registered

--- data_agg.py
+++ data_agg.py
@@ -43,12 +43,36 @@ class DataAgg:
 
 			previous_avg = np.stack([emos for emos in self.emotion_mem[-60:-30]])
 			previous_avg = np.mean(previous_avg, axis=0)
+			print("SHP", previous_avg.shape)
 
 			# Capture event if any emotion goes up signficantly or sentiment goes down
-			if np.any(np.abs(current_avg[1:] - previous_avg[1:]) > 1) or np.abs(current_avg[0] - previous_avg[0]) > 1.5:
+			emo = None
+			if np.abs(current_avg[0] - previous_avg[0]) > 3: # Sentiment changed
+				emo = "Overall sentiment"
+				bad = current_avg[0] < previous_avg[0]
+			elif np.abs(current_avg[1] - previous_avg[1]) > 1.5: # Happiness changed
+				emo = "Happiness"
+				bad = current_avg[0] < previous_avg[0]
+			elif np.abs(current_avg[2] - previous_avg[2]) > 1.5: # Sadness changed
+				emo = "Sadness"
+				bad = current_avg[0] > previous_avg[0]
+			elif np.abs(current_avg[3] - previous_avg[3]) > 1.5: # Anger changed
+				emo = "Anger"
+				bad = current_avg[0] > previous_avg[0]
+			elif np.abs(current_avg[4] - previous_avg[4]) > 1.5: # Fear changed
+				emo = "Fear"
+				bad = current_avg[0] > previous_avg[0]
+			elif np.abs(current_avg[5] - previous_avg[5]) > 1.5: # Disgust changed
+				emo = "Disgust"
+				bad = current_avg[0] > previous_avg[0]
+			elif np.abs(current_avg[6] - previous_avg[6]) > 1.5: # Suprise changed
+				emo = "Suprise"
+				bad = current_avg[0] > previous_avg[0]
+
+			if emo:
 				screenshot = pyautogui.screenshot().resize((512, 288))
 				screenshot = ImageTk.PhotoImage(image=screenshot)
-				self.events.append((len(self.emotion_mem) - 1, screenshot))
+				self.events.append((len(self.emotion_mem) - 1, screenshot, emo, bad))
 
 
 	def request_new_img(self,):
@@ -70,6 +94,14 @@ class DataAgg:
 			img = Image.fromarray(np.zeros((512, 288, 3)).astype(np.uint8)).resize((512, 288))
 			return ImageTk.PhotoImage(image=img)
 
+	def get_event_text(self, i):
+		if len(self.events) > i:
+			emotion = self.events[i][2]
+			bad = {False: "improved", True: "worsened"}[self.events[i][3]]
+			return "%s %s over the last 30 seconds" % (emotion, bad)
+		else:
+			return ""
+
 	def get_event_emo(self, i, emo_i):
 		if len(self.events) > i:
 			time = self.events[i][0]
@@ -84,36 +116,37 @@ class DataAgg:
 			return 0
 
 	def get_graph(self, live=True, cur_event=0):
-		print("graph")
 		plt.close()
 		fig, ax1 = plt.subplots(figsize=(7, 2.6))
+		fig.patch.set_facecolor("#EBEBEB")
 
 		if live:
-			sent_data = np.convolve(np.array(self.emotion_mem)[-240:, 0], np.ones((10,)), mode="same")/10
+			sent_data = np.convolve(np.array(self.emotion_mem)[-240:, 0], np.ones((3,)), mode="valid")/3
 			ax1.plot(0 - np.array(range(len(sent_data)))[::-1], sent_data, color="tab:blue", linewidth=3)
 		else:
-			sent_data = np.convolve(np.array(self.emotion_mem)[:, 0], np.ones((10,)), mode="same")/10
+			sent_data = np.convolve(np.array(self.emotion_mem)[:, 0], np.ones((3,)), mode="valid")/3
 			ax1.plot(range(len(sent_data)), sent_data, color="tab:blue", linewidth=3)
 
 		ax1.set_xlabel("Time (s)")
-		ax1.set_ylabel("Overall", color="tab:blue")
-		ax1.tick_params(axis='y', labelcolor="tab:blue")
-		ax1.set_ylim(-3, 3)
+		# ax1.set_ylabel("Overall", color="tab:blue")
+		ax1.tick_params(axis='y')
+		ax1.set_yticks([-3, 0, 3], ["V. Poor", "Avg", "V. Good"], rotation="vertical", verticalalignment="center")
+		ax1.set_ylim(-3.2, 3.2)
 
-		ax2 = ax1.twinx()
-		ax2.set_ylabel("Individual Emotions", color="black")
-		ax2.set_ylim(0, 3)
-
-		for i in range(6):
-			label = ["Happiness", "Sadness", "Anger", "Fear", "Disgust", "Suprise"][i]
-			color = ["tab:green", "tab:olive", "tab:orange", "tab:purple", "tab:brown", "tab:pink"][i]
-			if live:
-				sent_data = np.convolve(np.array(self.emotion_mem)[-240:, i + 1], np.ones((10,)), mode="same")/10
-				ax2.plot(0 - np.array(range(len(sent_data)))[::-1], sent_data, label=label, color=color)
-			else:
-				sent_data = np.convolve(np.array(self.emotion_mem)[:, i + 1], np.ones((10,)), mode="same")/10
-				ax2.plot(range(len(sent_data)), sent_data, label=label, color=color)
-		ax2.legend(loc="upper left")
+		# ax2 = ax1.twinx()
+		# ax2.set_ylabel("Individual Emotions", color="black")
+		# ax2.set_ylim(0, 3)
+		#
+		# for i in range(6):
+		# 	label = ["Happiness", "Sadness", "Anger", "Fear", "Disgust", "Suprise"][i]
+		# 	color = ["tab:green", "tab:olive", "tab:orange", "tab:purple", "tab:brown", "tab:pink"][i]
+		# 	if live:
+		# 		sent_data = np.convolve(np.array(self.emotion_mem)[-240:, i + 1], np.ones((10,)), mode="same")/10
+		# 		ax2.plot(0 - np.array(range(len(sent_data)))[::-1], sent_data, label=label, color=color)
+		# 	else:
+		# 		sent_data = np.convolve(np.array(self.emotion_mem)[:, i + 1], np.ones((10,)), mode="same")/10
+		# 		ax2.plot(range(len(sent_data)), sent_data, label=label, color=color)
+		# ax2.legend(loc="upper left")
 
 		if not live and len(self.events) > 0:
 			for i, event in enumerate(self.events):
@@ -132,10 +165,3 @@ class DataAgg:
 		img = ImageTk.PhotoImage(image=img)
 		buf.close()
 		return img
-
-
-
-
-
-
-
